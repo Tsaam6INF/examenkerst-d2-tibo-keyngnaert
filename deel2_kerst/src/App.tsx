c5ccbc986b4c582879edd195afadef2d9d8afba1
tibo-keyngnaert <Tibo.Keyngnaert@student.tsaam.be>
2024-12-02 10:13:27 +0100

    laatste versie, werkende app met juiste commentaar

--- deel2_kerst/src/App.tsx
+++ deel2_kerst/src/App.tsx
@@ -2,13 +2,13 @@
 import './App.css';
 import PersoonKaart from './personenKaart';
 
-interface Persoon {
-  naam: string;
+interface Persoon { // interface => blueprint van iets, zegt tegen programmeur : " maak een object van dit type"
+  naam: string;     // in dit geval dus van een string bijvoorbeeld
   klas: string[];
 }
 
 const App = () => {
-  const films: Persoon[] = [
+  const films: Persoon[] = [ // verwijzing naar de interface Persoon, om die variabelen te kunnen gebruiken
     { naam: "Karel Kleintjes", klas: ["1A", "2A", "3STW"] },
     { naam: "Els Dotjes", klas: ["1A", "2B", "3H"] },
   ];
@@ -17,13 +17,13 @@ const App = () => {
     <div className="app">
       <h1>Leerlingen School</h1>
       <div className="personen-container">
-        {films.map((film, index) => (
-          <PersoonKaart key={index} naam={film.naam} klas={film.klas} />
+        {films.map((film, index) => (  //map => voor elke persoon moeten deze elementen apart worden weergegeven
+          <PersoonKaart key={index} naam={film.naam} klas={film.klas} /> // in dit geval hun naam en klas, de key geeft een index aan elke persoon
         ))}
       </div>
     </div>
   );
 };
 
-export default App;
+export default App; // module exporteren om hem in je main.tsx te kunnen plaatsen
 
