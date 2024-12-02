72650def63bfd456ea022599b04d19de7f258d6d
tibo-keyngnaert <Tibo.Keyngnaert@student.tsaam.be>
2024-12-02 10:15:23 +0100

    De finale en correcte versie, in de vorige commit was ik iets vergeten

--- deel2_kerst/src/personenKaart.tsx
+++ deel2_kerst/src/personenKaart.tsx
@@ -1,20 +1,20 @@
 // src/MovieCard.tsx
 
-interface PersonenProps {
+interface PersonenProps { 
   naam: string;
   klas: string[];
 }
 
-const PersoonKaart = ({ naam, klas }: PersonenProps) => {
-  return (
+const PersoonKaart = ({ naam, klas }: PersonenProps) => { // het is tsx, dus definiëren dat dit van je interface hierboven komt
+  return ( 
     <div className="persoon-card">
       <div className="naam-persoon">
-        <h3>{naam}</h3>
+        <h3>{naam}</h3>     
       </div>
       <div className="persoon-klassen">
-        {klas.map((g, index) => (
-          <span key={index} className="persoon-tag">
-            {g}
+        {klas.map((klasnaam, index) => ( // voor ieder element apart een index geven, onderaan worden dan de klassen weergegeven 
+          <span key={index} className="klas-tag"> 
+            {klasnaam} 
           </span>
         ))}
       </div>
